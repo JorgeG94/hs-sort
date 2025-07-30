bcd43a0097c0366a314c99c012f48878a0ca1bc6
JorgeGV <jorgegalvez1694@gmail.com>
2025-07-30 21:17:41 +1000

    flowercase

--- src/hs_sort.f90
+++ src/hs_sort.f90
@@ -9,77 +9,77 @@ module hs_sort
 
 contains 
 
-subroutine HSORT(LIST, KEY)
+subroutine hsort(list, key)
   implicit none
-  integer, intent(inout) :: LIST(:)
-  double precision, intent(in) :: KEY(:)
+  integer, intent(inout) :: list(:)
+  double precision, intent(in) :: key(:)
 
-  integer, parameter :: MAXSTK = 32, NCUT = 12
-  integer :: LL, LR, LM, NL, NR, LTEMP, STKTOP, I, J, K
-  integer :: LSTACK(MAXSTK), RSTACK(MAXSTK)
-  double precision :: GUESS, VALUE
+  integer, parameter :: maxstk = 32, ncut = 12
+  integer :: ll, lr, lm, nl, nr, ltemp, stktop, i, j, k
+  integer :: lstack(maxstk), rstack(maxstk)
+  double precision :: guess, value
   logical :: quicksort_done
-  integer :: N 
-  N = size(LIST, 1)
-  LL = 1
-  LR = N
-  STKTOP = 0
+  integer :: n 
+  n = size(list, 1)
+  ll = 1
+  lr = n
+  stktop = 0
 
   quicksort_done = .false.
 
   do while (.not. quicksort_done)
 
-    if ((LR - LL) >= NCUT) then
-       NL = LL
-       NR = LR
-       LM = (LL + LR) / 2
-       GUESS = KEY(LIST(LM))
+    if ((lr - ll) >= ncut) then
+       nl = ll
+       nr = lr
+       lm = (ll + lr) / 2
+       guess = key(list(lm))
 
        do
-          do while (KEY(LIST(NL)) < GUESS)
-             NL = NL + 1
+          do while (key(list(nl)) < guess)
+             nl = nl + 1
           end do
 
-          do while (GUESS < KEY(LIST(NR)))
-             NR = NR - 1
+          do while (guess < key(list(nr)))
+             nr = nr - 1
           end do
 
-          if (NL < (NR - 1)) then
-             LTEMP = LIST(NL)
-             LIST(NL) = LIST(NR)
-             LIST(NR) = LTEMP
-             NL = NL + 1
-             NR = NR - 1
+          if (nl < (nr - 1)) then
+             ltemp = list(nl)
+             list(nl) = list(nr)
+             list(nr) = ltemp
+             nl = nl + 1
+             nr = nr - 1
           else
              exit
           end if
        end do
 
-       if (NL <= NR) then
-          if (NL < NR) then
-             LTEMP = LIST(NL)
-             LIST(NL) = LIST(NR)
-             LIST(NR) = LTEMP
+       if (nl <= nr) then
+          if (nl < nr) then
+             ltemp = list(nl)
+             list(nl) = list(nr)
+             list(nr) = ltemp
           end if
-          NL = NL + 1
-          NR = NR - 1
+          nl = nl + 1
+          nr = nr - 1
        end if
 
-       STKTOP = STKTOP + 1
-       if (NR < LM) then
-          LSTACK(STKTOP) = NL
-          RSTACK(STKTOP) = LR
-          LR = NR
+       stktop = stktop + 1
+       if (nr < lm) then
+          lstack(stktop) = nl
+          rstack(stktop) = lr
+          lr = nr
        else
-          LSTACK(STKTOP) = LL
-          RSTACK(STKTOP) = NR
-          LL = NL
+          lstack(stktop) = ll
+          rstack(stktop) = nr
+          ll = nl
        end if
 
-    else if (STKTOP /= 0) then
-       LL = LSTACK(STKTOP)
-       LR = RSTACK(STKTOP)
-       STKTOP = STKTOP - 1
+    else if (stktop /= 0) then
+       ll = lstack(stktop)
+       lr = rstack(stktop)
+       stktop = stktop - 1
 
     else
        quicksort_done = .true.
@@ -89,144 +89,144 @@ subroutine HSORT(LIST, KEY)
 
   ! Second pass: Insertion sort for final ordering
 
-  J = 1
-  K = LIST(1)
-  VALUE = KEY(K)
-  do I = 2, min(N, NCUT)
-     if (KEY(LIST(I)) < VALUE) then
-        J = I
-        VALUE = KEY(LIST(I))
+  j = 1
+  k = list(1)
+  value = key(k)
+  do i = 2, min(n, ncut)
+     if (key(list(i)) < value) then
+        j = i
+        value = key(list(i))
      end if
   end do
-  LIST(1) = LIST(J)
-  LIST(J) = K
+  list(1) = list(j)
+  list(j) = k
 
-  do I = 2, N
-     J = I
-     K = LIST(I)
-     VALUE = KEY(K)
-     do while (J > 1 .and. VALUE < KEY(LIST(J-1)))
-        LIST(J) = LIST(J-1)
-        J = J - 1
+  do i = 2, n
+     j = i
+     k = list(i)
+     value = key(k)
+     do while (j > 1 .and. value < key(list(j-1)))
+        list(j) = list(j-1)
+        j = j - 1
      end do
-     LIST(J) = K
+     list(j) = k
   end do
 
-end subroutine HSORT
+end subroutine hsort
 
-subroutine ISORT(LIST, KEY)
+subroutine isort(list, key)
   implicit none
-  integer, intent(inout) :: LIST(:)
-  double precision, intent(in) :: KEY(:)
+  integer, intent(inout) :: list(:)
+  double precision, intent(in) :: key(:)
 
-  integer :: N
-  integer :: I, J, K
-  double precision :: VALUE
+  integer :: n
+  integer :: i, j, k
+  double precision :: value
 
   ! Find the index with the minimum key and move it to the front
-  N = size(LIST, 1)
-  J = 1
-  K = LIST(1)
-  VALUE = KEY(K)
-  do I = 2, N
-     if (KEY(LIST(I)) < VALUE) then
-        J = I
-        VALUE = KEY(LIST(I))
+  n = size(list, 1)
+  j = 1
+  k = list(1)
+  value = key(k)
+  do i = 2, n
+     if (key(list(i)) < value) then
+        j = i
+        value = key(list(i))
      end if
   end do
-  LIST(1) = LIST(J)
-  LIST(J) = K
+  list(1) = list(j)
+  list(j) = k
 
   ! Insertion sort
-  do I = 2, N
-     J = I
-     K = LIST(I)
-     VALUE = KEY(K)
-     do while (J > 1 .and. VALUE < KEY(LIST(J-1)))
-        LIST(J) = LIST(J-1)
-        J = J - 1
+  do i = 2, n
+     j = i
+     k = list(i)
+     value = key(k)
+     do while (j > 1 .and. value < key(list(j-1)))
+        list(j) = list(j-1)
+        j = j - 1
      end do
-     LIST(J) = K
+     list(j) = k
   end do
 
-end subroutine ISORT
+end subroutine isort
 
-subroutine QSORT(LIST, KEY)
+subroutine qsort(list, key)
   implicit none
-  integer, intent(inout) :: LIST(:)
-  double precision, intent(in) :: KEY(:)
+  integer, intent(inout) :: list(:)
+  double precision, intent(in) :: key(:)
 
-  integer, parameter :: MAXSTK = 32
-  integer :: LL, LR, LM, NL, NR, LTEMP, STKTOP
-  integer :: LSTACK(MAXSTK), RSTACK(MAXSTK)
-  double precision :: GUESS
+  integer, parameter :: maxstk = 32
+  integer :: ll, lr, lm, nl, nr, ltemp, stktop
+  integer :: lstack(maxstk), rstack(maxstk)
+  double precision :: guess
   logical :: done
 
-  LL = 1
-  LR = size(LIST, 1)
-  STKTOP = 0
+  ll = 1
+  lr = size(list, 1)
+  stktop = 0
   done = .false.
 
   do while (.not. done)
-     if (LL < LR) then
-        NL = LL
-        NR = LR
-        LM = (LL + LR) / 2
-        GUESS = KEY(LIST(LM))
+     if (ll < lr) then
+        nl = ll
+        nr = lr
+        lm = (ll + lr) / 2
+        guess = key(list(lm))
 
         ! Partition the array
         do
-           do while (KEY(LIST(NL)) < GUESS)
-              NL = NL + 1
+           do while (key(list(nl)) < guess)
+              nl = nl + 1
            end do
 
-           do while (GUESS < KEY(LIST(NR)))
-              NR = NR - 1
+           do while (guess < key(list(nr)))
+              nr = nr - 1
            end do
 
-           if (NL < (NR - 1)) then
-              LTEMP = LIST(NL)
-              LIST(NL) = LIST(NR)
-              LIST(NR) = LTEMP
-              NL = NL + 1
-              NR = NR - 1
+           if (nl < (nr - 1)) then
+              ltemp = list(nl)
+              list(nl) = list(nr)
+              list(nr) = ltemp
+              nl = nl + 1
+              nr = nr - 1
            else
               exit
            end if
         end do
 
-        if (NL <= NR) then
-           if (NL < NR) then
-              LTEMP = LIST(NL)
-              LIST(NL) = LIST(NR)
-              LIST(NR) = LTEMP
+        if (nl <= nr) then
+           if (nl < nr) then
+              ltemp = list(nl)
+              list(nl) = list(nr)
+              list(nr) = ltemp
            end if
-           NL = NL + 1
-           NR = NR - 1
+           nl = nl + 1
+           nr = nr - 1
         end if
 
         ! Push the larger sub-list to the stack and continue with the smaller
-        STKTOP = STKTOP + 1
-        if (NR < LM) then
-           LSTACK(STKTOP) = NL
-           RSTACK(STKTOP) = LR
-           LR = NR
+        stktop = stktop + 1
+        if (nr < lm) then
+           lstack(stktop) = nl
+           rstack(stktop) = lr
+           lr = nr
         else
-           LSTACK(STKTOP) = LL
-           RSTACK(STKTOP) = NR
-           LL = NL
+           lstack(stktop) = ll
+           rstack(stktop) = nr
+           ll = nl
         end if
 
-     else if (STKTOP > 0) then
-        LL = LSTACK(STKTOP)
-        LR = RSTACK(STKTOP)
-        STKTOP = STKTOP - 1
+     else if (stktop > 0) then
+        ll = lstack(stktop)
+        lr = rstack(stktop)
+        stktop = stktop - 1
      else
         done = .true.
      end if
   end do
 
-end subroutine QSORT
+end subroutine qsort
 
 
 
